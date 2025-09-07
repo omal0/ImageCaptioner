7837dd337d07c4e12feefce26c45bf6a2dac31be
omal0 <nate.crivelli@gmail.com>
2025-09-06 23:00:15 -0700

    Frame grab testing
    Just tested out being able to grab a specific frame from a test video, will be able to implement this into working program later

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("hello")
+import cv2 # using opencv for image capturing
+
+# putting video file into object to be manipulated
+test = cv2.VideoCapture("TestFootage/carspassingby.mp4")
+
+# sets the frame we want to look at
+test.set(cv2.CAP_PROP_POS_FRAMES, 9)
+
+# reads the frame at the new position, ret is a boolean to test if the
+# VideoCapture object was actually done, frame is the actual frame if it worked
+ret, frame = test.read()
+
+# checking if .read() actually did anything
+if ret:
+    # saves frame to image file in Frames folder
+    cv2.imwrite("TestFootage/Frames/cartestframe10.jpg", frame)
+
+# stop working with video
+test.release()
