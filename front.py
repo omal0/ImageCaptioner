69402e68e1b33d7cf7317bc2ff67c629763680f8
atran742 <atran3420@sdsu.edu>
2025-10-10 14:04:36 -0700

    better front end

--- front.py
+++ front.py
@@ -16,7 +16,7 @@ def my_upload(): # show file browser and preview of photo
     filename = filedialog.askopenfilename(filetypes=f_types)
     img=ImageTk.PhotoImage(file=filename)
     b3=tk.Button(root,image=img) # display image on this
-    b3.grid(row=4,column=1,columnspan=3,pady=20)
+    b3.pack()
 def my_add():
     pass # code to store data in database
 
@@ -27,14 +27,18 @@ root.geometry("500x400")
 label = tk.Label(root, text="Please Input Video")
 label.pack(pady=20)
 
-button = tk.Button(root, text="Click me", command=on_button_click)
-button.pack()
+#button = tk.Button(root, text="Click me", command=on_button_click)
+#button.pack()
 
 b1=tk.Button(root,text='Upload',command=lambda:my_upload())
-b1.pack()
+b1.pack(side=tk.RIGHT, padx=10, pady=5)
 
 b2=tk.Button(root,text='Enter',command=lambda:my_add())
-b2.pack()
+
+b2.pack(side=tk.LEFT, padx=10, pady=5)
+
+result = tk.Label(root, text = 'The caption will go here')
+result.pack(side = tk.BOTTOM, padx = 20, pady = 15)
 
 global filename,img
 
