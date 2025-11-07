f4752ce9b77833e407575a8d82770571544a67b1
omal0 <nate.crivelli@gmail.com>
2025-11-07 14:07:55 -0800

    Back.py changed, going to work on it later

--- back.py
+++ back.py
@@ -5,7 +5,8 @@ import cv2
 import numpy as np
 import torch
 from PIL import Image
-from transformers import AutoProcessor, Blip2ForConditionalGeneration
+from transformers import BlipForConditionalGeneration, BlipProcessor
+import cv2
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using device: {device}")
@@ -17,16 +18,65 @@ elif device == "cpu":
     yolomodel = YOLO("model/yolo11n.pt").to(device)
 
 # Run prediction on an image
-results = yolomodel("TestFootage/carspassingby.mp4", show=True)  # show=True opens a window with boxes
+# results = yolomodel("TestFootage/carspassingby.mp4", show=True)  # show=True opens a window with boxes
+video_path = "TestFootage/carspassingby.mp4"
+cap = cv2.VideoCapture(video_path)
+frame_num = 0
 
-# Print detailed results
-for result in results:
-    boxes = result.boxes
-    for box in boxes:
-        print(f"Class: {model.keypoints[int(box.cls)]}, "
-              f"Conf: {float(box.conf):.2f}, "
-              f"Box: {box.xyxy.tolist()}")
-        
 # Blip Processing
-# processor = AutoProcessor.from_pretrained("Salesforce/blip2-opt-2.7b")
-# blipmodel = 
+processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+blip_model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+
+def describe_image(frame):
+    # Convert numpy frame -> PIL image
+    image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+    inputs = processor(images=image, return_tensors="pt").to("cuda")
+    out = blip_model.generate(**inputs)
+    caption = processor.decode(out[0], skip_special_tokens=True)
+    return caption
+
+if not cap.isOpened():
+    print("Error")
+else:
+    print("Start")
+
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if not ret:
+            break
+        
+        frame_num += 1
+
+        # Yolo results
+        results = yolomodel(frame, show=True)  # show=True opens a window with boxes 
+
+        # Blip Captions for frame
+        full_frame_caption = describe_image(frame)
+        print(full_frame_caption)
+
+        for result in results:
+
+            boxes = result.boxes
+
+            for box in boxes:
+               print(f"Class: {model.keypoints[int(box.cls)]}, "
+               f"Conf: {float(box.conf):.2f}, "
+               f"Box: {box.xyxy.tolist()}")
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+print("Done")
+
+
+# # Print detailed results
+# for result in results:
+
+#     boxes = result.boxes
+    
+#     # Drawing boxes on video
+#     for box in boxes:
+
+#         print(f"Class: {model.keypoints[int(box.cls)]}, "
+#               f"Conf: {float(box.conf):.2f}, "
+#               f"Box: {box.xyxy.tolist()}")
