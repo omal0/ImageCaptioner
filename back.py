5b44bd0648e8c31ca09bae921fcc2210152e60e2
omal0 <nate.crivelli@gmail.com>
2025-11-07 13:10:44 -0800

    new

--- back.py
+++ back.py
@@ -5,18 +5,19 @@ import cv2
 import numpy as np
 import torch
 from PIL import Image
-from transformers import BlipProcessor, BlipForConditionalGeneration
+from transformers import AutoProcessor, Blip2ForConditionalGeneration
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using device: {device}")
 
 # Load YOLOv8 model on GPU
-model = YOLO("model/yolo11x.pt").to(device)
-# Load this if you dont have cuda
-# model = YOLO("model/yolo11x.pt")
+if device == "cuda":
+    yolomodel = YOLO("model/yolo11x.pt").to(device)
+elif device == "cpu":
+    yolomodel = YOLO("model/yolo11n.pt").to(device)
 
 # Run prediction on an image
-results = model("TestFootage/carspassingby.mp4", show=True)  # show=True opens a window with boxes
+results = yolomodel("TestFootage/carspassingby.mp4", show=True)  # show=True opens a window with boxes
 
 # Print detailed results
 for result in results:
@@ -25,3 +26,7 @@ for result in results:
         print(f"Class: {model.keypoints[int(box.cls)]}, "
               f"Conf: {float(box.conf):.2f}, "
               f"Box: {box.xyxy.tolist()}")
+        
+# Blip Processing
+# processor = AutoProcessor.from_pretrained("Salesforce/blip2-opt-2.7b")
+# blipmodel = 
